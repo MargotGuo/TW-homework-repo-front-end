4b2e7b9a212d93e688daf2f5d5ebbd4c74c680f2
MargotGuo <gyqgdn@126.com>
2019-12-16 15:29:26 +0800

    [Guo Yuanqi]{refact}: modify the code

--- Week2-basic-JS/03-string/q4-toCamelStyle.js
+++ Week2-basic-JS/03-string/q4-toCamelStyle.js
@@ -37,7 +37,9 @@ var name1 = toCamelStyle('abc_bcd');  // 输出 'abcBcd'
 var name2 = toCamelStyle('a_3_c_d_ef');  // 输出 'a3CDEf'
 var name3 = toCamelStyle('_a_b_c_d_ef');  // 输出 '_aBCDEf
 var name4 = toCamelStyle('__a_b_c_d_ef');  // 输出 '__aBCDEf
+var name5 = toCamelStyle('__a_b___c_d_ef');  // 输出 '__aBCDEf
 console.log(name1);
 console.log(name2);
 console.log(name3);
 console.log(name4);
+console.log(name5);
