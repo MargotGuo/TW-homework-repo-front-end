7cf1bbb57943a52b5fa4fcf0bb6e9889bd08e56c
MargotGuo <gyqgdn@126.com>
2019-12-16 09:59:54 +0800

    [Guo Yuanqi]{refact}

--- Week2-basic-JS/04-array/q3-double.js
+++ Week2-basic-JS/04-array/q3-double.js
@@ -1,11 +1,3 @@
 var a = [1, 2, 3, 4, 5];
-var b = double(a);
+var b = a.map( x => x * 2);
 console.log(b);
-
-function double(inputArr) {
-  var outputArr = [];
-  for (var index = 0, len = inputArr.length; index < len; index++) {
-    outputArr[index] = inputArr[index] * 2;
-  }
-  return outputArr;
-}
