062d6b2e86d3cf4d563cbfbad64c7b23c2694643
MargotGuo <gyqgdn@126.com>
2019-12-20 20:49:00 +0800

    [Guo Yuanqi]{fix}: fix bugs

--- Week3-Functions-and-events/05-to-do-List/listAction.js
+++ Week3-Functions-and-events/05-to-do-List/listAction.js
@@ -1,7 +1,7 @@
-displayTask();
+displayAll();
 document.addEventListener("click", mouseAction);
 document.addEventListener("keypress", (event) => {
-  if (event.which === 13) { 
+  if (event.which === 13 && document.getElementById("confirm-task")) { 
     return addNewTask();
   }
 });
@@ -9,10 +9,10 @@ document.addEventListener("keypress", (event) => {
 function mouseAction(event) {
   if (event.target.id === "confirm-task") {
     return addNewTask();
-  } else if (event.target.name === "complete-task") {
-    return markAsComplete(event);
+  } else if (event.target.name === "task-done") {
+    return markAsDone(event);
   } else if (event.target.id === "all") {
-    return displayTask();
+    return displayAll();
   } else if (event.target.id === "active") {
     return displayActive();
   } else if (event.target.id === "complete") {
@@ -20,91 +20,25 @@ function mouseAction(event) {
   }
 }
 
-function displayTask() {
+function displayAll() {
   clearNode();
-  for (var index = 1; index <= localStorage.length; index++) {
-    var taskDetail = JSON.parse(localStorage.getItem(index));
-    addNewLine(index, taskDetail);
+  for (var index = 1, len = localStorage.length; index <= len; index++) {
+    var localTaskDetail = JSON.parse(localStorage.getItem(index));
+    displayNewTask(index, localTaskDetail);
   }
   if (document.getElementById("confirm-task-ban")) {
     document.getElementById("confirm-task-ban").setAttribute("id", "confirm-task");
   }
 }
 
-function addNewTask() {
-  var newIndex = document.getElementById("list").childNodes.length;
-  var newTaskContent = document.getElementById("task").value;
-  var maxIndex = localStorage.length + 1;
- if (newTaskContent) {
-    var newTaskDetail = {
-      "text": newTaskContent,
-      "checked": false
-    }
-    var newString = JSON.stringify(newTaskDetail);
-    localStorage.setItem(maxIndex, newString);
-    addNewLine(newIndex, newTaskDetail);
-    document.getElementById("task").value = "";
-  }
-}
-
-function addNewLine(newIndex, newTaskDetail) {
-  var taskLine = document.createElement("div");
-  document.getElementById("list").appendChild(taskLine);
-  taskLine.setAttribute("id", "line" + newIndex);
-  if (newIndex % 2) {
-    taskLine.setAttribute("class", "task-line");
-  } else {
-    taskLine.setAttribute("class", "task-line-colored");
-  }
-  if (newTaskDetail.checked) {
-    taskLine.innerHTML = 
-      "<p class='new-index-delete'>" + newIndex + ".</p>" +
-      "<input name='complete-task' type='checkbox' checked/>" +
-      "<p class='new-text-delete'>" + newTaskDetail.text + "</p>";
-  } else {
-    taskLine.innerHTML = 
-      "<p class='new-index'>" + newIndex + ".</p>" +
-      "<input name='complete-task' type='checkbox' />" +
-      "<p class='new-text'>" + newTaskDetail.text + "</p>";
-  }
-}
-
-function markAsComplete(event) {
-  var eventLine = event.target.parentNode;
-  if (event.target.checked === true) {
-    eventLine.childNodes[0].setAttribute("class", "new-index-delete");
-    eventLine.childNodes[2].setAttribute("class", "new-text-delete");
-  } else {
-    eventLine.childNodes[0].setAttribute("class", "new-index");
-    eventLine.childNodes[2].setAttribute("class", "new-text");
-  }
-  var completeId = eventLine.id.charAt(eventLine.id.length - 1);
-  var completeTask = JSON.parse(localStorage.getItem(completeId));
-  var completeStatus = completeTask.checked;
-  if (completeStatus) {
-    completeTask.checked = false;
-  } else {
-    completeTask.checked = true;
-  }
-  localStorage.setItem(completeId, JSON.stringify(completeTask));
-}
-
-function clearNode() {
-  var list = document.getElementById("list");
-  while (list.firstChild) {
-    list.removeChild(list.firstChild);
-  }
-  list.innerHTML = " ";
-}
-
 function displayActive() {
   clearNode();
-  var activeIndex = 0;
-  for (var index = 1; index <= localStorage.length; index++) {
-    var taskDetail = JSON.parse(localStorage.getItem(index));
-    if (taskDetail.checked === false) {
-      activeIndex++;
-      addNewLine(activeIndex, taskDetail);
+  var newIndex = 0;
+  for (var index = 1, len = localStorage.length; index <= len; index++) {
+    var localTaskDetail = JSON.parse(localStorage.getItem(index));
+    if (!localTaskDetail.checked) {
+      newIndex++;
+      displayNewTask(newIndex, localTaskDetail);
     }
   }
   if (document.getElementById("confirm-task-ban")) {
@@ -114,15 +48,83 @@ function displayActive() {
 
 function displayComplete() {
   clearNode();
-  var completeIndex = 0;
-  for (var index = 1; index <= localStorage.length; index++) {
-    var taskDetail = JSON.parse(localStorage.getItem(index));
-    if (taskDetail.checked === true) {
-      completeIndex++;
-      addNewLine(completeIndex, taskDetail);
+  var newIndex = 0;
+  for (var index = 1, len = localStorage.length; index <= len; index++) {
+    var localTaskDetail = JSON.parse(localStorage.getItem(index));
+    if (localTaskDetail.checked) {
+      newIndex++;
+      displayNewTask(newIndex, localTaskDetail);
     }
   }
   if (document.getElementById("confirm-task")) {
     document.getElementById("confirm-task").setAttribute("id", "confirm-task-ban");
   }
+}
+
+function addNewTask() {
+  var newTaskContent = document.getElementById("new-task").value;
+  if (newTaskContent) {
+    var newTaskIndex = localStorage.length + 1;
+    var newTaskDetail = {
+      id: newTaskIndex,
+      task: newTaskContent,
+      checked: false
+    }
+    var newTaskString = JSON.stringify(newTaskDetail);
+    localStorage.setItem(newTaskIndex, newTaskString);
+    var showIndex = document.getElementById("list").childNodes.length;
+    displayNewTask(showIndex, newTaskDetail);
+    document.getElementById("new-task").value = ""
+  }
+}
+
+function displayNewTask(index, taskStorage) {
+  var task = document.createElement("li");
+  task.setAttribute("id", taskStorage.id);
+  document.getElementById("list").appendChild(task);
+  if (index % 2) {
+    task.setAttribute("class", "task-item");
+  } else {
+    task.setAttribute("class", "task-item-colored");
+  }
+  if (taskStorage.checked) {
+    indexClass = "index-done";
+    contentClass = "task-content-done";
+  } else {
+    indexClass = "index";
+    contentClass = "task-content";
+  }
+  task.innerHTML = 
+    "<p class='" + indexClass + "'>" + index + "." + "</p>" +
+    "<input type='checkbox' name='task-done'/>" + 
+    "<p class='" + contentClass + "'>" + taskStorage.task + "</p>";
+  if (taskStorage.checked) {
+    task.childNodes[1].setAttribute("checked", true);
+  }
+}
+
+function markAsDone(event) {
+  var targetIndex = event.target.parentNode.id;
+  var targetList = document.getElementById(targetIndex);
+  var targetStorage = JSON.parse(localStorage.getItem(targetIndex));
+  var taskStatus = targetStorage.checked;
+  if (taskStatus) {
+    targetStorage.checked = false;
+    targetList.childNodes[0].setAttribute("class", "index");
+    targetList.childNodes[2].setAttribute("class", "task-content");
+  } else {
+    targetStorage.checked = true;
+    targetList.childNodes[0].setAttribute("class", "index-done");
+    targetList.childNodes[2].setAttribute("class", "task-content-done");
+  }
+  targetString = JSON.stringify(targetStorage);
+  localStorage.setItem(targetIndex, targetString);
+}
+
+function clearNode() {
+  var list = document.getElementById("list");
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+  list.innerHTML = " ";
 }
