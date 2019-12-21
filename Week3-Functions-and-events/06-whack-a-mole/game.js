9fa72f753306e8f3045d83ba5ae982fca9cf31c4
MargotGuo <gyqgdn@126.com>
2019-12-21 19:13:33 +0800

    [Guo Yuanqi]{feat}: AC[random mole] and AC[random time]

--- Week3-Functions-and-events/06-whack-a-mole/game.js
+++ Week3-Functions-and-events/06-whack-a-mole/game.js
@@ -13,24 +13,53 @@ window.onload = function () {
 
 
   startBtn.addEventListener('click', function () {
-    showBtnAnimation();
+    // showBtnAnimation();
     startGame();
   }, false);
 
-  function showBtnAnimation() {
-    event.preventDefault();
+  // function showBtnAnimation() {
+  //   event.preventDefault();
 
-    startBtn.classList.add('animate');
-    // 按钮动画延时，按钮动画结束后发生的事：换为正常状态（class中的animate去掉），开始按钮消失
-    setTimeout(() => {
-      startBtn.classList.remove('animate');
-      startBtn.style.display = 'none';
-    }, 700);
-  }
+  //   startBtn.classList.add('animate');
+  //   // 按钮动画延时，按钮动画结束后发生的事：换为正常状态（class中的animate去掉），开始按钮消失
+  //   setTimeout(() => {
+  //     startBtn.classList.remove('animate');
+  //     startBtn.style.display = 'none';
+  //   }, 700);
+  // }
 
 
   function startGame() {
-      // TODO: 写开始新游戏后发生的事
+    // TODO: 写开始新游戏后发生的事
+    var intervalID = window.setInterval(molePeep, peepTime() + 1000);
   }
 
+  function molePeep() {
+    var index = peepHole();
+    var targetHole = document.getElementsByClassName("hole")[index];
+    var presentClass = targetHole.getAttribute("class");
+    if (presentClass.split(" ").indexOf("up") === -1) {
+      presentClass += " up";
+    }
+    targetHole.setAttribute("class", presentClass);
+    setTimeout(moleHide, peepTime());
+  }
+
+  function moleHide() {
+    var moleHole = document.getElementsByClassName("up")[0];
+    var presentClass = moleHole.getAttribute("class");
+    presentClass = presentClass.slice(0, 10);
+    moleHole.setAttribute("class", presentClass);
+  }
+
+  function peepHole() {
+    var totalHoleNumber = document.getElementsByClassName("hole").length;
+    var holeIndex = Math.floor(Math.random() * totalHoleNumber);
+    return holeIndex;
+  }
+
+  function peepTime() {
+    var keepTime = 300 + Math.random() * 600;
+    return keepTime;
+  }
 };
