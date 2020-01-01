3070bdd00393fa5ab895433761181f68b5592a9f
MargotGuo <gyqgdn@126.com>
2020-01-01 17:18:09 +0800

    [Guo Yuanqi]{feat}: answer for q1, layout for [huang-men-ji] index

--- Week5-Basic-ES6-OOP/01-Basic-ES6/q2-take-out-food/public/index.js
+++ Week5-Basic-ES6-OOP/01-Basic-ES6/q2-take-out-food/public/index.js
@@ -1,12 +1,70 @@
-// 请把与index.html页面相关的javascript代码写在这里
-// 同时删除该注释
+displayMenu();
+displayPromotion();
+document.getElementById("calculate").addEventListener("click", calculatePrice);
+document.getElementById("clear").addEventListener("click", clear);
 
-function calculatePrice() {
-  // 想办法调用`bestCharge`并且把返回的字符串
-  // 显示在html页面的`message`中
+function displayMenu() {
+  document.getElementById("items").innerHTML = `
+  <tr>
+    <th>序号</th>
+    <th>菜品代号</th>
+    <th>菜品</th>
+    <th>价格</th>
+    <th>数量</th>
+  </tr>`;
+  let menu = loadAllItems();
+  menu.forEach((item) => {
+    document.getElementById("items").innerHTML += `
+    <tr>
+      <td>${menu.indexOf(item) + 1}</td>
+      <td>${item.id}</td>
+      <td>${item.name}</td>
+      <td>${item.price}</td>
+      <td><input name="count" type="text", value=0 id="${menu.indexOf(item)}"></td>
+    </tr>`;
+  });
 }
 
+function displayPromotion() {
+  document.getElementById("promotions").innerHTML = `
+  <tr>
+    <th>序号</th>
+    <th>优惠种类</th>
+    <th>详情</th>
+  </tr>`;
+  let promotion = loadPromotions();
+  promotion.forEach((item) => {
+    document.getElementById("promotions").innerHTML += `
+    <tr>
+      <td>${promotion.indexOf(item) + 1}</td>
+      <td>${item.type}</td>
+      <td>${(item.items) ? item.items : ""}</td>
+    </tr>`
+  });
+}
+
+function calculatePrice() {
+  let menu = loadAllItems();
+  menu.forEach((item, currentIndex) => {
+    itemCount = document.getElementById(currentIndex).value;
+    menu[currentIndex].count = itemCount;
+  });
+  let selectedItems = [];
+  menu.forEach((item) => {
+    if (item.count !== "0") {
+      let info = `${item.id} x ${item.count}`
+      selectedItems.push(info);
+    };
+  });
+
+  let outputInfo = bestCharge(selectedItems);
+  return outputInfo;
+}
+
+
 function clear() {
-  // 清除用户的选择，以及页面显示的信息
-  // 清除之后，用户可以继续正常使用各项功能
+  document.getElementById("message").innerHTML = "";
+  document.getElementsByName("count").forEach((item) => {
+    item.value = 0;
+  });
 }
--- Week5-Basic-ES6-OOP/01-Basic-ES6/q2-take-out-food/src/best-charge.js
+++ Week5-Basic-ES6-OOP/01-Basic-ES6/q2-take-out-food/src/best-charge.js
@@ -1,3 +1,64 @@
+const menuInfo = getMenuInfo();
+
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+  console.log(selectedItems);
+  let result;
+  if (isPromotionA(selectedItems) && isPromotionB(selectedItems)) {
+    console.log("a & b")
+  } else {
+    if (isPromotionA(selectedItems)) {
+      console.log("a");
+    } else if (isPromotionB(selectedItems)) {
+      console.log("b");
+    } else {
+      console.log("no");
+    }
+  }
+  return result;
+}
+
+function calculatePlanA(itemArr) {
+}
+
+function calculatePlanB(itemArr) {
+}
+
+function calculatePrice(itemArr) {
+}
+
+function compare(itemArr) {
+}
+
+function isPromotionA(itemArr) {
+  let sum = itemArr.reduce((tempSum, currentItem) => {
+    let productID = currentItem.split("x")[0].trim();
+    let productCount = currentItem.split("x")[1].trim();
+    return tempSum + Number(productCount) * menuInfo[productID].price;
+  }, 0);
+  if (sum >= 30) {
+    return true;
+  }
+  return false;
+}
+
+function isPromotionB(itemArr) {
+  let specifiedProduct = itemArr.filter((currentItem) => {
+    let productID = currentItem.split("x")[0].trim();
+    return ['ITEM0001', 'ITEM0022'].includes(productID);
+  });
+  if (specifiedProduct.length) {
+    return true;
+  }
+  return false;
+}
+
+function getMenuInfo() {
+  let menuArray = loadAllItems()
+  let outputInfo = {};
+  menuArray.forEach((item) => {
+    outputInfo[item.id] = {};
+    outputInfo[item.id].name = item.name;
+    outputInfo[item.id].price = item.price;
+  });
+  return outputInfo;
 }
