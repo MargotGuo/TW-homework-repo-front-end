e4022e92776b102e83dc217fb549fe4deee0fed0
MargotGuo <gyqgdn@126.com>
2020-01-04 22:54:05 +0800

    [Guo Yuanqi]{refactor}: for [take-out-food] in OOP way

---
+++ Week5-Basic-ES6-OOP/01-Basic-ES6/q2-take-out-food/src/best-charge-back-up.js
@@ -0,0 +1,131 @@
+let getMenuInfo = () => {
+  let menuArray = loadAllItems();
+  let outputInfo = {};
+  menuArray.forEach((item) => {
+    outputInfo[item.id] = {};
+    outputInfo[item.id].name = item.name;
+    outputInfo[item.id].price = item.price;
+  });
+  return outputInfo;
+};
+
+const menuInfo = getMenuInfo();
+
+let bestCharge = (selectedItems) => {
+  let result;  
+  if (isPlanA(selectedItems) && isPlanB(selectedItems)) {
+    result = compare(selectedItems);
+  } else {
+    if (isPlanA(selectedItems)) {
+      result = adoptPlanA(selectedItems);
+    } else if (isPlanB(selectedItems)) {
+      result = adoptPlanB(selectedItems);
+    } else if (selectedItems.length === 0) {
+      result = "您没有选择菜品";
+    } else {
+      result = noDiscount(selectedItems);
+    }
+  }
+  return result;
+};
+
+let isPlanA = (food) => {
+  let sum = getSum(food);
+  if (sum >= 30) {
+    return true;
+  }
+  return false;
+};
+
+let isPlanB = (food) => {
+  let foodInDiscount = getDiscountFood(food);
+  if (foodInDiscount.length) {
+    return true;
+  }
+  return false;
+};
+
+let getDiscountFood = (food) => {
+  let foodInDiscount = food.filter((currentFood) => {
+    let foodId = currentFood.split("x")[0].trim();
+    return ["ITEM0001", "ITEM0022"].includes(foodId);
+  });
+  return foodInDiscount;
+};
+
+let getSum = (food) => {
+  let sum = food.reduce((tempSum, currentFood) => {
+    let foodId = currentFood.split("x")[0].trim();
+    let count = Number(currentFood.split("x")[1]);
+    return tempSum + count * menuInfo[foodId].price;
+  }, 0);
+  return sum;
+};
+
+let getFoodDetail = (food) => {
+  let detail = "";
+  food.forEach((item) => {
+    let foodId = item.split("x")[0].trim();
+    let count = Number(item.split("x")[1]);
+    detail += `${menuInfo[foodId].name} x ${count} = ${menuInfo[foodId].price * count}元\n`;
+  });
+  detail = detail.substring(0, detail.length - 1);
+  return detail;
+};
+
+function showFinalInfo() {
+  let detail = getFoodDetail(arguments[0]);
+  return `============= 订餐明细 =============
+${detail}
+-----------------------------------
+${arguments[2] ? arguments[2] + 
+  "\n-----------------------------------\n" :""}总计：${arguments[1]}元
+===================================`;
+}
+
+let adoptPlanA = (food) => {
+  let sum = getSum(food) - 6;
+  let discountInfo = `使用优惠:
+满30减6元，省6元`;
+  return showFinalInfo(food, sum, discountInfo);
+};
+
+let adoptPlanB = (food) => {
+  let foodInDiscount = getDiscountFood(food);
+  let discountInfo = `使用优惠:
+指定菜品半价(${getDiscountNameB(foodInDiscount)})，省${getDiscountB(foodInDiscount)}元`;
+  let sum = getSum(food) - getDiscountB(foodInDiscount);
+  return showFinalInfo(food, sum, discountInfo);
+};
+
+let compare = (food) => {
+  let discountB = getDiscountB(getDiscountFood(food));
+  if (discountB > 6) {
+    return adoptPlanB(food);
+  }
+  return adoptPlanA(food);
+};
+
+let noDiscount = (food) => {
+  let sum = getSum(food);
+  return showFinalInfo(food, sum);
+};
+
+let getDiscountB = (food) => {
+  let discount = food.reduce((tempDiscount, currentFood) => {
+    let foodId = currentFood.split("x")[0].trim();
+    let foodCount = Number(currentFood.split("x")[1]);
+    return tempDiscount + foodCount * 0.5 * menuInfo[foodId].price;
+  }, 0);
+  return discount;
+};
+
+let getDiscountNameB = (food) => {
+  let name = "";
+  food.forEach((currentFood) => {
+    let foodId = currentFood.split("x")[0].trim();
+    name += `${menuInfo[foodId].name}，`;
+  });
+  name = name.substring(0, name.length - 1);
+  return name;
+};
--- Week5-Basic-ES6-OOP/01-Basic-ES6/q2-take-out-food/src/best-charge.js
+++ Week5-Basic-ES6-OOP/01-Basic-ES6/q2-take-out-food/src/best-charge.js
@@ -1,4 +1,4 @@
-let getMenuInfo = () => {
+const menuInfo = (() => {
   let menuArray = loadAllItems();
   let outputInfo = {};
   menuArray.forEach((item) => {
@@ -7,125 +7,116 @@ let getMenuInfo = () => {
     outputInfo[item.id].price = item.price;
   });
   return outputInfo;
-};
-
-const menuInfo = getMenuInfo();
+})();
 
 let bestCharge = (selectedItems) => {
-  let result;  
-  if (isPlanA(selectedItems) && isPlanB(selectedItems)) {
-    result = compare(selectedItems);
+  let result;
+  let planA = new PlanA(selectedItems);
+  let planB = new PlanB(selectedItems);
+  let plan = new Plan(selectedItems);
+
+  if (planA.isInDiscount() && planB.isInDiscount()) {
+    result = compare(planA, planB);
   } else {
-    if (isPlanA(selectedItems)) {
-      result = adoptPlanA(selectedItems);
-    } else if (isPlanB(selectedItems)) {
-      result = adoptPlanB(selectedItems);
+    if (planA.isInDiscount()) {
+      result = planA.getOutputInfo();
+    } else if (planB.isInDiscount()) {
+      result = planB.getOutputInfo();
     } else if (selectedItems.length === 0) {
       result = "您没有选择菜品";
     } else {
-      result = noDiscount(selectedItems);
+      result = plan.getOutputInfo();
     }
   }
   return result;
 };
 
-let isPlanA = (food) => {
-  let sum = getSum(food);
-  if (sum >= 30) {
-    return true;
+let compare = (planA, planB) => {
+  return (planA.discount >= planB.discount) ? planA.getOutputInfo() : planB.getOutputInfo();
+};
+
+class Plan {
+  constructor(food) {
+    this.food = this.getFood(food);
+    this.sum = this.getSum();
+    this.finalPrice = this.sum;
+    this.foodDetail = (() => {
+      let detail = "";
+      for (let key in this.food) {
+        detail += `${menuInfo[key].name} x ${this.food[key]} = ${menuInfo[key].price * this.food[key]}元\n`;
+      }
+      detail = detail.substring(0, detail.length - 1);
+      return detail;
+    })();
   }
-  return false;
-};
 
-let isPlanB = (food) => {
-  let foodInDiscount = getDiscountFood(food);
-  if (foodInDiscount.length) {
-    return true;
+  getFood(food) {
+    let foodObj = {};
+    food.forEach((currentFood) => {
+      let foodId = currentFood.split("x")[0].trim();
+      let count = Number(currentFood.split("x")[1]);
+      foodObj[foodId] = count;
+    });
+    return foodObj;
   }
-  return false;
-};
 
-let getDiscountFood = (food) => {
-  let foodInDiscount = food.filter((currentFood) => {
-    let foodId = currentFood.split("x")[0].trim();
-    return ["ITEM0001", "ITEM0022"].includes(foodId);
-  });
-  return foodInDiscount;
-};
+  getSum() {
+    let sum = 0;
+    for(let key in this.food) {
+      sum += menuInfo[key].price * this.food[key];
+    }
+    return sum;
+  }
 
-let getSum = (food) => {
-  let sum = food.reduce((tempSum, currentFood) => {
-    let foodId = currentFood.split("x")[0].trim();
-    let count = Number(currentFood.split("x")[1]);
-    return tempSum + count * menuInfo[foodId].price;
-  }, 0);
-  return sum;
-};
-
-let getFoodDetail = (food) => {
-  let detail = "";
-  food.forEach((item) => {
-    let foodId = item.split("x")[0].trim();
-    let count = Number(item.split("x")[1]);
-    detail += `${menuInfo[foodId].name} x ${count} = ${menuInfo[foodId].price * count}元\n`;
-  });
-  detail = detail.substring(0, detail.length - 1);
-  return detail;
-};
-
-function showFinalInfo() {
-  let detail = getFoodDetail(arguments[0]);
-  return `============= 订餐明细 =============
-${detail}
+  getOutputInfo() {
+    return `============= 订餐明细 =============
+${this.foodDetail}
 -----------------------------------
-${arguments[2] ? arguments[2] + 
-  "\n-----------------------------------\n" :""}总计：${arguments[1]}元
+${this.discount ? this.discountDetail + 
+"\n-----------------------------------\n" :""}总计：${this.finalPrice}元
 ===================================`;
+  }
 }
 
-let adoptPlanA = (food) => {
-  let sum = getSum(food) - 6;
-  let discountInfo = `使用优惠:
+class PlanA extends Plan {
+  constructor(food) {
+    super(food);
+    this.discount = 6;
+    this.finalPrice -= this.discount;
+    this.discountDetail = `使用优惠:
 满30减6元，省6元`;
-  return showFinalInfo(food, sum, discountInfo);
-};
-
-let adoptPlanB = (food) => {
-  let foodInDiscount = getDiscountFood(food);
-  let discountInfo = `使用优惠:
-指定菜品半价(${getDiscountNameB(foodInDiscount)})，省${getDiscountB(foodInDiscount)}元`;
-  let sum = getSum(food) - getDiscountB(foodInDiscount);
-  return showFinalInfo(food, sum, discountInfo);
-};
-
-let compare = (food) => {
-  let discountB = getDiscountB(getDiscountFood(food));
-  if (discountB > 6) {
-    return adoptPlanB(food);
   }
-  return adoptPlanA(food);
-};
 
-let noDiscount = (food) => {
-  let sum = getSum(food);
-  return showFinalInfo(food, sum);
-};
+  isInDiscount() {
+    return (this.getSum(this.food) >= 30) ? true : false;
+  }
+}
 
-let getDiscountB = (food) => {
-  let discount = food.reduce((tempDiscount, currentFood) => {
-    let foodId = currentFood.split("x")[0].trim();
-    let foodCount = Number(currentFood.split("x")[1]);
-    return tempDiscount + foodCount * 0.5 * menuInfo[foodId].price;
-  }, 0);
-  return discount;
-};
+class PlanB extends Plan {
+  constructor(food) {
+    super(food);
+    this.discount = (() => {
+      let discount = 0;
+      for (let key in this.food) {
+        discount += (key === "ITEM0001" || key === "ITEM0022") ? (this.food[key] * menuInfo[key].price * 0.5) : 0;
+      }
+      return discount;
+    })();
+    this.finalPrice -= this.discount;
+    this.discountDetail = `使用优惠:
+指定菜品半价(${this.getDiscountName()})，省${this.discount}元`;
+  }
 
-let getDiscountNameB = (food) => {
-  let name = "";
-  food.forEach((currentFood) => {
-    let foodId = currentFood.split("x")[0].trim();
-    name += `${menuInfo[foodId].name}，`;
-  });
-  name = name.substring(0, name.length - 1);
-  return name;
-};
+  isInDiscount() {
+    return ("ITEM0001" in this.food || "ITEM0022" in this.food) ? true : false;
+  }
+
+  getDiscountName() {
+    let name = "";
+    for (let key in this.food) {
+      name += (key === "ITEM0001" || key === "ITEM0022") ? `${menuInfo[key].name}，` : "";
+    }
+    name = name.substring(0, name.length - 1);
+    return name;
+  }
+}
